da4566feeb3ba1596ac41b5f1d98a74fd83c3215
4anghyeon <uphyeon@kakao.com>
2026-04-29 17:10:20 +0900

    refactor: simplify MenuList indexing logic

--- src/shared/constant/menu-items.ts
+++ src/shared/constant/menu-items.ts
@@ -1,4 +1,4 @@
 export const MENU_ITEMS = [
-  { index: 0, to: "/", subPath: "/ko/post", label: "Posts" },
-  { index: 1, to: "/about", label: "About" },
+  { to: "/", subPath: "/ko/post", label: "Posts" },
+  { to: "/about", label: "About" },
 ] as const;
